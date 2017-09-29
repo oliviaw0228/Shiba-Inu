0cebc37cf5a5e105d2afdadf399a884376cd3c48
oliviawy0228 <olivia0228@icloud.com>
2017-09-28 22:33:12 -0700

    First Commit

---
+++ Shiba Inu/DataSource.swift
@@ -0,0 +1,33 @@
+//
+//  DataSource.swift
+//  Shiba Inu
+//
+//  Created by Olivia on 2017-09-28.
+//  Copyright © 2017 Olivia. All rights reserved.
+//
+
+import Foundation
+
+struct DataSource {
+  let boys = ["Hoshi": "http://www.calgaryshibas.com/files/262/thumb/DSC_0226.png",
+              "Samurai": "http://www.calgaryshibas.com/files/276/thumb/_MG_0075-WM.png",
+              "Dai Kokoro": "http://www.calgaryshibas.com/files/292/thumb/Scan10001.png",
+              "Maverick": "http://www.calgaryshibas.com/files/467/thumb/Maverick-9111.png",
+              "Mustang": "http://www.calgaryshibas.com/files/484/thumb/Mustang-9133.png",
+              "Stetson": "http://www.calgaryshibas.com/files/489/thumb/Stetson-9206.png"]
+  
+  let girls = ["Bandit": "http://www.calgaryshibas.com/files/400/thumb/Bandit-lowres-9707.png?1330280669",
+               "Shomi": "http://www.calgaryshibas.com/files/207/thumb/lange-0583.png?1237683371",
+               "Cheemo": "http://www.calgaryshibas.com/files/303/thumb/_MG_0035WM-1.png?1290273646",
+               "Scout": "http://www.calgaryshibas.com/files/286/thumb/IMG_0613.png?1289796528",
+               "Cougar": "http://www.calgaryshibas.com/files/428/thumb/Cougar-6848.png?1354587746",
+               "Maple": "http://www.calgaryshibas.com/files/415/thumb/Maplelowres-3359.png?1349031885",
+               "Lacey": "http://www.calgaryshibas.com/files/470/thumb/Lacey-9168.png?1386458458",
+               "Fancy": "http://www.calgaryshibas.com/files/473/thumb/Daisy-9376b.png?1386458979",
+               "Poppy": "http://www.calgaryshibas.com/files/493/thumb/Poppy-9918.png?1398535345",
+               "Brambleberry": "http://www.calgaryshibas.com/files/499/thumb/Brambleberry-8788.png?1437769960",
+               "Foxy": "http://www.calgaryshibas.com/files/524/thumb/Foxy-9902.png?1449237156",
+               "Sky": "http://www.calgaryshibas.com/files/511/thumb/Sky-9810.png?1449000546",
+               "Stormy": "http://www.calgaryshibas.com/files/516/thumb/Stormy-9822.png?1449001770",
+               "Juniperberry": "http://www.calgaryshibas.com/files/522/thumb/Juniper-9906.png?1449237023"]
+}
--- Shiba Inu/ViewController.swift
+++ Shiba Inu/ViewController.swift
@@ -9,17 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
 }
 
+class DogCell: UITableViewCell {
+  @IBOutlet weak var name: UILabel!
+  @IBOutlet weak var dogImageView: UIImageView!
+}
+
+class DogTableViewController: UITableViewController {
+  
+  let boys = DataSource().boys
+  
+  // height
+  override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 100.0
+  }
+  
+  // how many rows to display
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return self.boys.count
+  }
+  
+  // what does each row look like
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "DoggyCell") as! DogCell
+    
+    // set up cell name
+    let dogName = Array(self.boys.keys)[indexPath.row]
+    cell.name.text = dogName
+    
+    // set up cell image
+    DispatchQueue.global().async { // download image in background
+      let imageUrlString = self.boys[dogName]!
+      let url = URL(string: imageUrlString)
+      let data = try? Data(contentsOf: url!)
+      
+      DispatchQueue.main.async { // download in main thread will block UI
+        cell.dogImageView.image = UIImage(data: data!)
+      }
+    }
+  return cell
+  }
+
+}
