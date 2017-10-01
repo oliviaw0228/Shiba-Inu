0471fb583716ddff1812127be9c12841b552e33d
oliviawy0228 <olivia0228@icloud.com>
2017-09-30 19:10:56 -0700

    addTitle

--- Shiba Inu/ViewController.swift
+++ Shiba Inu/ViewController.swift
@@ -9,14 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+  var dogTitle: String?
+  
   @IBAction func buttonPressed(_ sender: UIButton) {
     let data = (sender.titleLabel?.text == "Boys") ? DataSource().boys : DataSource().girls
+    self.dogTitle = sender.titleLabel?.text
     performSegue(withIdentifier: "showDogs", sender: data)
   }
   
   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
     if segue.identifier == "showDogs" {
       let dogsTVC = segue.destination as? DogTableViewController
+      dogsTVC?.navigationItem.title = self.dogTitle
       dogsTVC?.dogs = sender as! [String:String]
     }
   }
